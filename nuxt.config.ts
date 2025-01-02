4f488dd6fc783a134ae00c6c25dfc1071f6a1349
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-03 10:07:24 +1300

    Chore: bump @nuxt/ui-pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui-pro", "@nuxt/eslint"],
   css: ['~/assets/css/main.css'],
 
   future: {
