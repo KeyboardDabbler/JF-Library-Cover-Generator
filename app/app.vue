0f6aee66aeffeccaf808abd5ff206e30474b8264
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-03 15:54:19 +1300

    Style: UFooter component

--- app/app.vue
+++ app/app.vue
@@ -5,22 +5,27 @@
 <template>
   <UApp>
     <Html class="scheme-only-dark scrollbar-thin" dir="ltr">
+
     <Body class="antialiased duration-300 transition-colors">
       <NuxtLoadingIndicator />
       <div class="flex flex-col min-h-screen">
         header
         <div class="flex-1 w-full flex flex-col">
           <div class="relative flex-1 flex flex-col mx-auto max-w-8xl w-full h-full">
-            <UMain>
-              <UContainer>
-                tetststsdtfsugfgsfjygsdfjh
-              </UContainer>
-            </UMain>
+            <UContainer>
+              tetststsdtfsugfgsfjygsdfjh
+            </UContainer>
           </div>
         </div>
-        footer
+        <UFooter>
+          <p class="my-3">
+            Made with ❤️ by <NuxtLink to="https://github.com/KeyboardDabbler" class="text-yellow-400 font-bold">
+              keyboardDabbler</NuxtLink>
+          </p>
+        </UFooter>
       </div>
     </Body>
+
     </Html>
-  </UApp>
+  </UApp>s
 </template>
