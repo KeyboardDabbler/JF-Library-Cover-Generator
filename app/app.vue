e4b57f470fecff0ceb8336627e88ca40273ace33
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-04 20:17:28 +1300

    Fix: remove to link

--- app/app.vue
+++ app/app.vue
@@ -309,7 +309,7 @@ function downloadImage() {
           <UFooter>
             <p class="my-3 text-dimText">
               Made with ❤️ by <NuxtLink
-                to="https://github.com/KeyboardDabbler"
+                to=""
                 class="text-yellow-400 font-bold"
               >
                 keyboardDabbler</NuxtLink>
