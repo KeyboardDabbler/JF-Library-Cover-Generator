5220b3ff20e9ff23016d82474eb1bc98c28c6c8f
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-04 23:39:32 +1300

    Chore: disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui-pro', '@nuxt/eslint'],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
 
   future: {
