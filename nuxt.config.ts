0f28f92b245fe5b445d4cb25498066487b0d9766
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-05 16:01:12 +1300

    Chore: install @nuxt/scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui-pro', '@nuxt/eslint'],
+  modules: ['@nuxt/ui-pro', '@nuxt/eslint', '@nuxt/scripts'],
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
 
